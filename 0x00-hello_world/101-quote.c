fb7fccf95de6f9012c1caeff4869fe6e35b41eab
Leavano Goliath <leavanogoliath@gmail.com>
2023-09-07 12:55:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main - Enrty point
- *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
