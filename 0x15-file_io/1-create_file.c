4ce1dca7d44e766b1f50d55aaf1e262fd70296d7
90D74TH3R <leavanogoliath@gmail.com>
2023-10-30 09:55:48 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,10 @@
 
 /**
  * create_file - creates a file
- * @filename: filename
- * @text_content: content writed in the file
- * Return: 1 if (Success), -1 if (Fails)
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -23,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters];  nletters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
 	rwr = write(fd, text_content, nletters);
 
@@ -33,5 +35,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 
 	return (1);
-
 }
